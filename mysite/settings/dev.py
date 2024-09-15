a1b3e1541bc2e339e972ab6ad1f5965d0a47defb
Shapon Sheikh <shsheikhbd@gmail.com>
2024-09-15 23:58:51 +0600

    added new project files

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-)pbizd%a9uoq+drf1@&i@$3rqk-u)pp%3sxp%tr2t&@(dekk1m"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
