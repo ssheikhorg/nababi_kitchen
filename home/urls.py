6a338c39331e1b98ae7cc8599888eb5fced0c4b7
Shapon Sheikh <shsheikhbd@gmail.com>
2024-09-17 19:14:49 +0600

    added contact and about page, made it dynamic

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, AboutUsView, ContactView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about/', AboutUsView.as_view(), name='about'),
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -20,3 +20,17 @@ class HomeView(TemplateView):
         context['contact_info'] = contact_info
         context['gallery_images'] = gallery_images
         return context
+
+
+class AboutUsView(TemplateView):
+    template_name = 'home/about.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'home/contact.html'
+
+    def get_context_data(self, **kwargs):
+        contact_info = ContactInfo.objects.first()
+        context = super().get_context_data(**kwargs)
+        context['contact_info'] = contact_info
+        return context
